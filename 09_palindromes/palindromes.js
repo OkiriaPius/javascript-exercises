f9f0d055185fe61c06f60f88ba0dd2f44c21e7c7
OkiriaPius <okiriapiusicloud@gmail.com>
2024-11-08 09:09:52 +0300

    this exercise brings mind to deep thinking and problem solving aspect in finding out possible solutions and the research

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let answers = string.toString().replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    let answer = answers.split('').reverse().join('');
+    if (answer === answers) {
+        return true
+    }else {
+        return false;
+    };
 };
 
 // Do not edit below this line
