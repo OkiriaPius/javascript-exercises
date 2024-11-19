0bff191f2cefd1f89bab6401f64516884dd6b48c
OkiriaPius <okiriapiusicloud@gmail.com>
2024-11-19 23:09:56 +0300

    array method exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    //use map because it returns a new array according to condition
+    const foundTitles = books.map((book)=>{
+        //condition
+        return book.title;
+    });
+    //console.log(foundTitles);
+    return foundTitles; 
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let currentYear = (new Date()).getFullYear();
+    
+    
+  const logestYears = Math.max(...people.map((nextPerson) => {
+        
+      if (nextPerson.yearOfDeath === undefined) {
+        return currentYear - nextPerson.yearOfBirth 
+      } 
+      if (nextPerson.yearOfDeath !== undefined) {
+        return nextPerson.yearOfDeath - nextPerson.yearOfBirth
+      }
+  }));
+    //console.log(logestYears);
+    return people.find((person) => {
+      if (person.yearOfDeath === undefined) {
+        return currentYear - person.yearOfBirth === logestYears
+      }
+      if (person.yearOfDeath !== undefined) {
+        return person.yearOfDeath - person.yearOfBirth === logestYears
+      }
+  });
+    
+    
 };
 
 // Do not edit below this line
